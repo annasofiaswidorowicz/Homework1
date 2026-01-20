29136a67c81c92e82ad212997f74b0ef6f5d77e6
annasofiaswidorowiczlara <annasofiaswidorowicz@gmail.com>
2026-01-20 13:56:00 +0100

    HW1 ood or even code added

--- HW1.py
+++ HW1.py
@@ -2,3 +2,11 @@ name = input("Enter your name: ")
 print("hi", name)
 print(5 + 5)
 print("Welcome to my first Python homework!")
+
+n = int(input("Enter a number: "))
+if n % 2 == 0:
+    print("This number is even")
+else:
+    print("This number is odd")
+
+
